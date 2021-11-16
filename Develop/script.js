f53e498f2998ecb2d54606f9e029a6a9651259e1
DrDano <danielharned@gmail.com>
2021-11-16 11:13:35 -0800

    adding arrays of lower and upper case characters

--- Develop/script.js
+++ Develop/script.js
@@ -11,6 +11,8 @@ function writePassword() {
   // character variables defined here
   var charNumbers = [1,2,3,4,5,6,7,8,9]
   var charSpecial = ["!","\"","#","$","%","&","\'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","\`","{","|","}","~"]
+  var charUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var charLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   //define function generatePassword which will be the final step in generating the password
   // series of if evaluators to determine which characters the user desires, using randCharCount to determine how many of each character to include.
