1e306d9f03f882a9d7f0b1381104b597918341f3
DrDano <danielharned@gmail.com>
2021-11-16 13:23:10 -0800

    rearranging code so that all functions are defined before execution

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  
 
   // character variables defined here
   var charNumbers = [1,2,3,4,5,6,7,8,9];
@@ -14,8 +11,15 @@ function writePassword() {
   var charUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var charLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  
-  var charCounter = function(pwLength) {
+  // user decisions defined here
+  var confirmCharLen = window.prompt("How many characters do you want your password to be? [8-128]");
+  var confirmCharNum = confirm("Would you like numbers in your password?");
+  var confirmCharSpecial = confirm("Would you like special characters in your password?");
+  var confirmCharUpper = confirm("Would you like upper case letters in your password?");
+  var confirmCharLower = confirm("Would you like lower case letters in your password?");
+
+
+  function charCounter(pwLength) {
     var charTypes = 0
     if (confirmCharNum) {charTypes += 1}
     if (confirmCharSpecial) {charTypes += 1}
@@ -33,7 +37,7 @@ function writePassword() {
   }
 
 
-  var passwordCharSelector = function(charTypeCount) {
+  function pwCharSelector(charTypeCount) {
     var password = ""
     if (confirmCharNum) {
       for (var i=0; i<charTypeCount; i++) {
@@ -64,29 +68,20 @@ function writePassword() {
   // series of if evaluators to determine which characters the user desires, using randCharCount to determine how many of each character to include.
   // if totalCharCount > confirmCharCount Then 
   function generatePassword() {
-    debugger;
-    var confirmCharNum = confirm("Would you like numbers in your password?");
-    var confirmCharSpecial = confirm("Would you like special characters in your password?");
-    var confirmCharUpper = confirm("Would you like upper case letters in your password?");
-    var confirmCharLower = confirm("Would you like lower case letters in your password?");
+    
 
-    passwordCharSelector(charCounter);
+    return pwCharSelector(charCounter(confirmCharLen));
 
   };
 
-  // function to output number of each array of characters to use in password
-
-  
-
-
   // prompt user for input of 8-128 characters
-  var confirmCharLen = window.prompt("How many characters do you want your password to be? [8-128]");
-
-
- 
 
   // create copy to clipboard button
 
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 
 
 }
