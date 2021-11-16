892ed1afd9d9088e6ddce67981c69fd133bb215b
DrDano <danielharned@gmail.com>
2021-11-16 10:06:17 -0800

    adding initial comments in script to guide construction

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,21 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //define function generatePassword which will be the final step in generating the password
+
+
+  // prompt user for input of 8-128 characters
+
+  // prompt user with confirm alerts to allow use of variety of characters
+    // confirm use of numbers
+    // confirm use of special characters
+    // confirm use of upper case characters
+    // confirm use of lower case characters
+
+  // create copy to clipboard button
+
+
+
 }
 
 // Add event listener to generate button
