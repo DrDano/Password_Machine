0293ec276e034a01a03e24c93cbfb83816e74647
DrDano <danielharned@gmail.com>
2021-11-16 12:22:43 -0800

    rearranging code to allow generatePassword to utilize functions

--- Develop/script.js
+++ Develop/script.js
@@ -14,22 +14,7 @@ function writePassword() {
   var charUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var charLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  //define function generatePassword which will be the final step in generating the password
-  // series of if evaluators to determine which characters the user desires, using randCharCount to determine how many of each character to include.
-  // if totalCharCount > confirmCharCount Then 
-  function generatePassword() {
-    confirmCharLen
-    confirmCharNum
-    confirmCharSpecial
-    confirmCharUpper
-    confirmCharLower
-
-    
-
-    return 1
-  };
-
-  // function to output number of each array of characters to use in password
+  
   var charCounter = function(pwLength) {
     var charTypes = 0
     if (confirmCharNum) {charTypes += 1}
@@ -41,50 +26,64 @@ function writePassword() {
     return pwLength/charTypes
   }
 
+
   var randomIndex = function(indexLen) {
     var randInd = (indexLen * Math.random);
     return randInd;
   }
 
+
   var passwordCharSelector = function(charTypeCount) {
     var password = ""
     if (confirmCharNum) {
       for (var i=0; i<charTypeCount; i++) {
-        password += charNumbers[randomIndex]
+        password += charNumbers[randomIndex(charNumbers.length)]
       };
     }
     if (confirmCharSpecial) {
       for (var i=0; i<charTypeCount; i++) {
-        password += charSpecial[randomIndex]
+        password += charSpecial[randomIndex(charSpecial.length)]
       };
     }
     if (confirmCharUpper) {
       for (var i=0; i<charTypeCount; i++) {
-        password += charUpperCase[randomIndex]
+        password += charUpperCase[randomIndex(charUpperCase.length)]
       };
     }
     if (confirmCharLower) {
       for (var i=0; i<charTypeCount; i++) {
-        password += charLowerCase[randomIndex]
+        password += charLowerCase[randomIndex(charLowerCase.length)]
+      }
     }
-
     return password;
   }
+
+
+
+  //define function generatePassword which will be the final step in generating the password
+  // series of if evaluators to determine which characters the user desires, using randCharCount to determine how many of each character to include.
+  // if totalCharCount > confirmCharCount Then 
+  function generatePassword() {
+    debugger;
+    var confirmCharNum = confirm("Would you like numbers in your password?");
+    var confirmCharSpecial = confirm("Would you like special characters in your password?");
+    var confirmCharUpper = confirm("Would you like upper case letters in your password?");
+    var confirmCharLower = confirm("Would you like lower case letters in your password?");
+
+    passwordCharSelector(charCounter);
+
+  };
+
+  // function to output number of each array of characters to use in password
+
+  
 
 
   // prompt user for input of 8-128 characters
   var confirmCharLen = window.prompt("How many characters do you want your password to be? [8-128]");
 
 
-  // prompt user with confirm alerts to allow use of variety of characters
-    // confirm use of numbers
-  var confirmCharNum = confirm("Would you like numbers in your password?");
-    // confirm use of special characters
-  var confirmCharSpecial = confirm("Would you like special characters in your password?");
-    // confirm use of upper case characters
-  var confirmCharUpper = confirm("Would you like upper case letters in your password?");
-    // confirm use of lower case characters
-  var confirmCharLower = confirm("Would you like lower case letters in your password?");
+ 
 
   // create copy to clipboard button
 
@@ -95,16 +94,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-
-
-
-
-
-
-  // random number generator to determine how many of each character desired by user are in password
-  var randCharCount = function(userCharNum) {
-    var charNum = randomNumber(8, userCharNum);
-    return charNum;
-  }
